b1ab9fa4308e3ea042fe1037332326d84043e6e3
Chris Grimmett <chris@grimtech.net>
2014-09-06 01:51:04 -0700

    go yeoman!

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vanmanAndyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vanmanAndyApp
+ */
+angular.module('vanmanAndyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
